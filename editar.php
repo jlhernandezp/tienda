31d3002a78c39ead60ba1ac4afe89d83c73a4b1c
JL <JL@Dell>
2017-03-25 12:19:13 +0100

    version final tienda

--- editar.php
+++ editar.php
@@ -7,25 +7,30 @@ and open the template in the editor.
 -->
     <head>
         <meta charset="UTF-8">
+        <link href="dwes.css" rel="stylesheet" type="text/css">
         <title>Editar</title>
     </head>
     <body>
-  
-        <form name='edicion' method="post" action="actualizar.php">
-            <?php 
-                echo "<input type='hidden' name='cod' value='".$_POST['cod']."' />";
-            ?>
-            <label>Producto:</label><br/>
-            <input type='text' name='nombreCorto' value='<?php echo $_POST['nombre_corto']; ?>' /><br/>
-            <label>Nombre</label><br/>
-            <input type='text' name='nombre' value='<?php echo $_POST['nombre']; ?>'/><br/>
-            <label>Descripción</label><br/>
-            <input type='textarea' name='descripcion' value='<?php echo $_POST['descripcion']; ?>'/><br/>
-            <label>PVP</label><br/>
-            <input type='text' name='pvp' size='25'value='<?php echo $_POST['pvp']; ?>' /><br/>
-            <input type='submit' name='actualizar' value='Actualizar'/>
-            <input type='submit' name='cancelar' value='Cancelar'/>
-            
-        </form>
+        <div id="encabezado">
+            <h1>Tarea:Edición de un producto: </h1>
+        </div>
+        <div id="contenido">
+            <form name='edicion' method="post" action="actualizar.php">
+                <?php 
+                    echo "<input type='hidden' name='cod' value='".$_POST['cod']."' />";
+                ?>
+                <h2><label>Producto:</label><br/></h2>
+                <input type='text' name='nombreCorto' size="100" value='<?php echo $_POST['nombre_corto']; ?>' /><br/>
+                <label>Nombre:</label><br/>
+                <input type='text' name='nombre' size="100" value='<?php echo $_POST['nombre']; ?>'/><br/>
+                <label>Descripción:</label><br/>
+                <textarea   name='descripcion' rows="9" cols="100" wrap="soft"><?php echo $_POST['descripcion']; ?></textarea><br/>
+                <label>PVP:</label><br/>
+                <input type='text' name='pvp' size='8'value='<?php echo $_POST['pvp']; ?>' /><br/>
+                <input type='submit' name='actualizar' value='Actualizar'/>
+                <input type='submit' name='cancelar' value='Cancelar'/>
+
+            </form>
+        </div>
     </body>
 </html>
